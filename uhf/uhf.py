4e266c7d0c76f7dccc19de7842bdd2df79028592
Avery Wiens <aewiens@uga.edu>
2017-01-31 12:42:46 -0500

    project 1 UHF -- done

--- uhf/uhf.py
+++ uhf/uhf.py
@@ -1,39 +1,65 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
-import psi4, numpy as np
-from   scipy import linalg as la
+import psi4, numpy as np, configparser
+from  scipy import linalg as la
 
-class UHF:
 
-    def __init__(self,mol,mints):
+class UHF(object):
 
-		##  integrals
+	def __init__(self,config):
+
+		config.read('Options.ini')
+		self.maxit = int(config['SCF']['maxIter'])
+		molecule   = psi4.geometry( config['DEFAULT']['molecule'] )
+
+		molecule.update_geometry()
+
+		mult     = molecule.multiplicity()
+		nelec    = self.get_nelec(molecule)       
+		self.Vnu = molecule.nuclear_repulsion_energy()
+		self.Na  = int( 0.5*(nelec+mult-1) )
+		self.Nb  = nelec - self.Na
+		self.E   = 0
+		self.mol = molecule
+
+
+	def get_nelec(self,mol):
+		char = mol.molecular_charge()
+		nelec = -char
+		for A in range(mol.natom()):
+			nelec += mol.Z(A)
+		return int(nelec)
+
+
+	def loadIntegrals(self):
+
+		basis   = psi4.core.BasisSet.build(self.mol, "BASIS", config['DEFAULT']['basis'])
+		mints   = psi4.core.MintsHelper(basis)
+
 		V = np.array( mints.ao_potential() )
 		T = np.array( mints.ao_kinetic() )
 		G = np.array( mints.ao_eri() )
-
-		##  psi things
-		mult       = mol.multiplicity()
-		nelec      = get_nelec(mol)       
-		self.conv  = psi4.get_global_option('E_CONVERGENCE')
-		self.maxit = psi4.get_option('SCF','MAXITER')
 
 		##  class variables for UHF iterations
 		self.S     = np.array( mints.ao_overlap() )
 		self.Hcore = T + V
 		self.G     = G.transpose((0,2,1,3))
 		self.X     = np.matrix( la.funm(self.S, lambda x : x**(-0.5) ) )
-		self.Vnu   = mol.nuclear_repulsion_energy()
-
-		self.Na    = int( 0.5*(nelec+mult-1) )
-		self.Nb    = nelec - self.Na
 		self.Da    = np.zeros(self.X.shape)
 		self.Db    = np.zeros(self.X.shape)
-		self.E     = 0
 
-    def compute_energy(self):
+		return None
 
-		Hcore, G, Da, Db, X = self.Hcore, self.G, self.Da, self.Db, self.X
+
+	def compute_energy(self):
+
+		self.loadIntegrals()
+
+		H  = self.Hcore
+		G  = self.G
+		Da = self.Da
+		Db = self.Db
+		X  = self.X
 
 		self.converged = False
 		for i in range( self.maxit ):
@@ -41,8 +67,8 @@ class UHF:
 			va = np.einsum("mnrs,ns->mr",G,Da) - np.einsum("mnsr,ns->mr",G,Da) + np.einsum("mnrs,ns->mr",G,Db)
 			vb = np.einsum("mnrs,ns->mr",G,Db) - np.einsum("mnsr,ns->mr",G,Db) + np.einsum("mnrs,ns->mr",G,Da)
 
-			Fa = Hcore + va
-			Fb = Hcore + vb
+			Fa = H + va
+			Fb = H + vb
 
 			tFa = X.dot(Fa.dot(X))
 			tFb = X.dot(Fb.dot(X))
@@ -60,35 +86,36 @@ class UHF:
 			Db = oCb.dot(oCb.T)
 
 			E0 = self.E
-			E = np.trace( (Hcore+0.5*va).dot(Da) ) + np.trace( (Hcore+0.5*vb).dot(Db) ) + self.Vnu
+			E = np.trace( (H+0.5*va).dot(Da) ) + np.trace( (H+0.5*vb).dot(Db) ) + self.Vnu
 			dE = np.fabs(E-E0)
 
 			print("UHF  {:>4} {: >21.11}  {: >21.11}".format(i,E,dE))
 
-			if dE < self.conv:
+			if dE < 1e-12:
 				self.converged = True
 				break
 
 			##  save
-			self.Da, self.Db, self.E = Da, Db, E
+			self.Da = Da
+			self.Db = Db
+			self.E  = E
 
 		self.write_output()
-		return self.E
-
-    def write_output(self):
-        if self.converged:
-            print("\nUHF procedure has converged.\nFinal UHF energy:{:20.11f}".format(self.E) )
-
-        if not self.converged:
-            print("UHF procedure did not converge. Sorry")
-
-        return None
+		return E
 
 
-def get_nelec(mol):
+	def write_output(self):
+		if self.converged:
+			print("\nUHF procedure has converged.\nFinal UHF energy:{:20.11f}".format(self.E) )
 
-    char = mol.molecular_charge()
-    nelec = -char
-    for A in range(mol.natom()):
-        nelec += mol.Z(A)
-    return int(nelec)
+		if not self.converged:
+			print("UHF procedure did not converge. Sorry")
+
+		return None
+
+
+if __name__ == '__main__':
+
+	config = configparser.ConfigParser()
+	uhf    = UHF(config)
+	uhf.compute_energy()
