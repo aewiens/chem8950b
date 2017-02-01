7cd8bbf46ce6142bdd71322c72aeefd28e5dea9d
Avery Wiens <aewiens@uga.edu>
2017-01-31 22:59:49 -0500

    cleaned up uhf class

--- uhf/uhf.py
+++ uhf/uhf.py
@@ -6,16 +6,10 @@ from  scipy import linalg as la
 
 class UHF(object):
 
-	def __init__(self,config):
-
-		config.read('Options.ini')
-		self.maxit = int(config['SCF']['maxIter'])
-		molecule   = psi4.geometry( config['DEFAULT']['molecule'] )
-
-		molecule.update_geometry()
+	def __init__(self,molecule,mints):
 
 		mult     = molecule.multiplicity()
-		nelec    = self.get_nelec(molecule)       
+		nelec    = self.getNelec(molecule)       
 		self.Vnu = molecule.nuclear_repulsion_energy()
 		self.Na  = int( 0.5*(nelec+mult-1) )
 		self.Nb  = nelec - self.Na
@@ -23,7 +17,7 @@ class UHF(object):
 		self.mol = molecule
 
 
-	def get_nelec(self,mol):
+	def getNelec(self,mol):
 		char = mol.molecular_charge()
 		nelec = -char
 		for A in range(mol.natom()):
@@ -33,14 +27,10 @@ class UHF(object):
 
 	def loadIntegrals(self):
 
-		basis   = psi4.core.BasisSet.build(self.mol, "BASIS", config['DEFAULT']['basis'])
-		mints   = psi4.core.MintsHelper(basis)
-
 		V = np.array( mints.ao_potential() )
 		T = np.array( mints.ao_kinetic() )
 		G = np.array( mints.ao_eri() )
 
-		##  class variables for UHF iterations
 		self.S     = np.array( mints.ao_overlap() )
 		self.Hcore = T + V
 		self.G     = G.transpose((0,2,1,3))
@@ -51,7 +41,7 @@ class UHF(object):
 		return None
 
 
-	def compute_energy(self):
+	def computeEnergy(self):
 
 		self.loadIntegrals()
 
@@ -62,7 +52,7 @@ class UHF(object):
 		X  = self.X
 
 		self.converged = False
-		for i in range( self.maxit ):
+		for i in range(100):
 
 			va = np.einsum("mnrs,ns->mr",G,Da) - np.einsum("mnsr,ns->mr",G,Da) + np.einsum("mnrs,ns->mr",G,Db)
 			vb = np.einsum("mnrs,ns->mr",G,Db) - np.einsum("mnsr,ns->mr",G,Db) + np.einsum("mnrs,ns->mr",G,Da)
@@ -100,22 +90,28 @@ class UHF(object):
 			self.Db = Db
 			self.E  = E
 
-		self.write_output()
+		self.writeOutput()
 		return E
 
 
-	def write_output(self):
+	def writeOutput(self):
+
 		if self.converged:
 			print("\nUHF procedure has converged.\nFinal UHF energy:{:20.11f}".format(self.E) )
-
 		if not self.converged:
 			print("UHF procedure did not converge. Sorry")
-
-		return None
 
 
 if __name__ == '__main__':
 
 	config = configparser.ConfigParser()
-	uhf    = UHF(config)
-	uhf.compute_energy()
+	config.read('Options.ini')
+
+	molecule   = psi4.geometry( config['DEFAULT']['molecule'] )
+	molecule.update_geometry()
+
+	basis = psi4.core.BasisSet.build(molecule, "BASIS", config['DEFAULT']['basis'])
+	mints = psi4.core.MintsHelper(basis)
+
+	uhf   = UHF(molecule,mints)
+	uhf.computeEnergy()
