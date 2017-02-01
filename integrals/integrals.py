7061ffe18570f558cea2fdbec87c32a6f0a47bf9
Avery Wiens <aewiens@uga.edu>
2017-01-31 23:05:20 -0500

    messed with things

--- integrals/integrals.py
+++ integrals/integrals.py
@@ -1,39 +1,19 @@
 #!/usr/bin/env python
 import psi4, numpy as np
-from collections import namedtuple
-
 import configparser
-config = configparser.ConfigParser()
-config.read('Options.ini')
-
-molecule = psi4.geometry(config['DEFAULT']['molecule'])
-basis    = psi4.core.BasisSet.build(molecule, 'BASIS', 'STO-3G', puream=0)
-mints    = psi4.core.MintsHelper(basis)
-nShell   = basis.nshell() 
-nbf      = basis.nao() 
-
-recursionResults = namedtuple('recursionResults',['x','y','z'])
 
 
 def osRecursion(PA, PB, alpha, AMa, AMb):
 	if len(PA) != 3 or len(PB) != 3:
 		raise ""
 
-	"""
-	##  Allocate x, y, and z matrices
-	x = np.zeros((AMa+1, AMb+1))
-	y = np.zeros((AMa+1, AMb+1))
-	z = np.zeros((AMa+1, AMb+1))
-	"""
-
-	##  Perform recursion
-
-	xyz = []
+	xyz   = []
 	coeff = 1.0
 
+	##  Perform recursion
 	for i in range(3):
 		I = np.zeros((AMa+1, AMb+1))
-		I[0,0] = 1.0
+		I[0,0] = coeff
 
 		for a in range(1,AMa+1):
 			I[a,0] = PA[i]*I[a-1,0]
@@ -54,10 +34,17 @@ def osRecursion(PA, PB, alpha, AMa, AMb):
 		xyz.append(I)
 	return xyz
 
-XYZ = osRecursion([1.0, 2.0, 3.0], [3.0, 2.0, 1.0], 5.0, 3, 3)
 
-print('x=\n', XYZ[0])
-print('y=\n', XYZ[1])
-print('z=\n', XYZ[2])
+if __name__ == '__main__':
 
-print( type(XYZ[0]))
+	config = configparser.ConfigParser()
+	config.read('Options.ini')
+
+	molecule = psi4.geometry(config['DEFAULT']['molecule'])
+	basis    = psi4.core.BasisSet.build(molecule, 'BASIS', 'STO-3G', puream=0)
+	mints    = psi4.core.MintsHelper(basis)
+	XYZ = osRecursion([1.0, 2.0, 3.0], [3.0, 2.0, 1.0], 5.0, 3, 3)
+
+	print('x=\n', XYZ[0])
+	print('y=\n', XYZ[1])
+	print('z=\n', XYZ[2])
