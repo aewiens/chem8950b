33f860ea58134802c485ff74d543773ad6a128be
Avery Wiens <aewiens@uga.edu>
2021-11-27 12:25:16 -0500

    Added diis to integral options

--- integrals/integrals.py
+++ integrals/integrals.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
-
 import sys, configparser, psi4, numpy as np
 from index import amIndices
-
-sys.path.append('../uhf/')
 from uhf import UHF
 
-class OS(object):
+
+class Integrals:
 
 	def __init__(self,basis,molecule,uhfObject):
 
@@ -14,94 +12,13 @@ class OS(object):
 		self.mol   = molecule
 		self.uhf   = uhfObject
 		self.S     = np.zeros(( basis.nao(),basis.nao() ))
-		self.T     = np.zeros_like(self.S)
-		self.dx    = np.zeros_like(self.S)
-		self.dy    = np.zeros_like(self.S)
-		self.dz    = np.zeros_like(self.S)
-
-		print('The number of shells: %d' % (basis.nshell()))
-		print('The number of basis functions: %d' % (basis.nao()))
-
-		for ishell in range(basis.nshell()):
-			shell = basis.shell(ishell)
-			print('Shell %d:' % (ishell))
-			print('  Atom: %d' % (shell.ncenter))
-			print('  AM: %d' % (shell.am))
-			print('  # Cartesian functions: %d' % (shell.ncartesian))
-			print('  # of primitive Gaussians: %d ' % (shell.nprimitive))
-			print('  function_index: %d' % (shell.function_index))
-			print('  center: %f, %f, %f' % (molecule.x(shell.ncenter), molecule.y(shell.ncenter), molecule.z(shell.ncenter)))
-
-
-	def fillIndices(self,AM):
-		
-		indexObject = amIndices(AM)
-		indexArray  = indexObject.indices
-		indexList   = []
-		for row in indexArray:
-			indexList.append( [ int(i) for i in row ] )
-		return indexList
-
-
-	def computeKinetic(self):
-
-		icount = 0
-		for ishell in range(self.basis.nshell()):
-			aShell = self.basis.shell(ishell)
-			nbfA   = (aShell.am+1)*(aShell.am+2)//2
-
-			for i in range(nbfA):
-				jcount = 0
-
-				for jshell in range(self.basis.nshell()):
-					bShell = self.basis.shell(jshell)
-					nbfB   = (bShell.am+1)*(bShell.am+2)//2
-
-					for j in range(nbfB):
-
-						for aprimitive in range(aShell.nprimitive):
-							for bprimitive in range(bShell.nprimitive):
-
-								alphaA = aShell.exp(aprimitive)
-								alphaB = bShell.exp(bprimitive)
-								alpha  = alphaA + alphaB
-								zeta   = alphaA*alphaB/(alphaA+alphaB)
-
-								A      = np.array( [mol.x(aShell.ncenter), mol.y(aShell.ncenter) ,mol.z(aShell.ncenter)] )
-								B      = np.array( [mol.x(bShell.ncenter), mol.y(bShell.ncenter) ,mol.z(bShell.ncenter)] )
-								P      = (alphaA*A + alphaB*B)/(alphaA+alphaB)
-
-								Ca     = aShell.coef(aprimitive)
-								Cb     = bShell.coef(bprimitive)
-
-								kappa  = np.exp( -zeta*np.linalg.norm(A-B)**2 )
-								KK     = kappa*(np.pi/alpha)**(3/2)
-
-								XYZ    = self.osRecursion(P-A,P-B,alpha,aShell.am+1,bShell.am+1)
-								x,y,z  = XYZ[0], XYZ[1], XYZ[2]
-
-								aIndices = self.fillIndices(aShell.am)[i]
-								bIndices = self.fillIndices(bShell.am)[j]
-								l1, l2   = aIndices[0], bIndices[0]
-								m1, m2   = aIndices[1], bIndices[1]
-								n1, n2   = aIndices[2], bIndices[2]
-
-								Tx = 0.5*y[m1,m2]*z[n1,n2]
-								Ty = 0.5*x[l1,l2]*z[n1,n2]
-								Tz = 0.5*x[l1,l2]*y[m1,m2]
-							
-								Tx *= l1*l2*x[l1-1,l2-1] + 4*alphaA*alphaB*x[l1+1,l2+1] - 2*alphaA*l2*x[l1+1,l2-1] - 2*alphaB*l1*x[l1-1,l2+1]
-								Ty *= m1*m2*y[m1-1,m2-1] + 4*alphaA*alphaB*y[m1+1,m2+1] - 2*alphaA*m2*y[m1+1,m2-1] - 2*alphaB*m1*y[m1-1,m2+1]
-								Tz *= n1*n2*z[n1-1,n2-1] + 4*alphaA*alphaB*z[n1+1,n2+1] - 2*alphaA*n2*z[n1+1,n2-1] - 2*alphaB*n1*z[n1-1,n2+1]
-
-								self.T[icount,jcount] += KK*Ca*Cb*(Tx+Ty+Tz)
-
-						jcount += 1
-				icount += 1
-
-		return self.T
-
-
+		self.T     = np.zeros_like( self.S )
+		self.dx    =  np.zeros(( basis.nao(), basis.nao() ))
+		self.dy    = np.zeros_like(self.dx)
+		self.dz    = np.zeros_like(self.dx)
+		self.os    = OS(basis,molecule,uhfObject)
+	
+	
 	def computeOverlap(self):
 
 		basis  = self.basis
@@ -138,11 +55,11 @@ class OS(object):
 								kappa  = np.exp( -zeta*np.linalg.norm(A-B)**2 )
 								KK     = kappa*(np.pi/alpha)**(3/2)
 
-								XYZ    = self.osRecursion(P-A,P-B,alpha,aShell.am,bShell.am)
+								XYZ    = self.os.recursion(P-A,P-B,alpha,aShell.am,bShell.am)
 								x,y,z  = XYZ[0], XYZ[1], XYZ[2]
 
-								aIndices = self.fillIndices(aShell.am)[i]
-								bIndices = self.fillIndices(bShell.am)[j]
+								aIndices = self.os.fillIndices(aShell.am)[i]
+								bIndices = self.os.fillIndices(bShell.am)[j]
 								l1, l2   = aIndices[0], bIndices[0]
 								m1, m2   = aIndices[1], bIndices[1]
 								n1, n2   = aIndices[2], bIndices[2]
@@ -153,6 +70,65 @@ class OS(object):
 				icount += 1
 
 		return self.S
+
+
+	def computeKinetic(self):
+
+		icount = 0
+		for ishell in range(self.basis.nshell()):
+			aShell = self.basis.shell(ishell)
+			nbfA   = (aShell.am+1)*(aShell.am+2)//2
+
+			for i in range(nbfA):
+				jcount = 0
+
+				for jshell in range(self.basis.nshell()):
+					bShell = self.basis.shell(jshell)
+					nbfB   = (bShell.am+1)*(bShell.am+2)//2
+
+					for j in range(nbfB):
+
+						for aprimitive in range(aShell.nprimitive):
+							for bprimitive in range(bShell.nprimitive):
+
+								alphaA = aShell.exp(aprimitive)
+								alphaB = bShell.exp(bprimitive)
+								alpha  = alphaA + alphaB
+								zeta   = alphaA*alphaB/(alphaA+alphaB)
+
+								A      = np.array( [mol.x(aShell.ncenter), mol.y(aShell.ncenter) ,mol.z(aShell.ncenter)] )
+								B      = np.array( [mol.x(bShell.ncenter), mol.y(bShell.ncenter) ,mol.z(bShell.ncenter)] )
+								P      = (alphaA*A + alphaB*B)/(alphaA+alphaB)
+
+								Ca     = aShell.coef(aprimitive)
+								Cb     = bShell.coef(bprimitive)
+
+								kappa  = np.exp( -zeta*np.linalg.norm(A-B)**2 )
+								KK     = kappa*(np.pi/alpha)**(3/2)
+
+								XYZ    = self.os.recursion(P-A,P-B,alpha,aShell.am+1,bShell.am+1)
+								x,y,z  = XYZ[0], XYZ[1], XYZ[2]
+
+								aIndices = self.os.fillIndices(aShell.am)[i]
+								bIndices = self.os.fillIndices(bShell.am)[j]
+								l1, l2   = aIndices[0], bIndices[0]
+								m1, m2   = aIndices[1], bIndices[1]
+								n1, n2   = aIndices[2], bIndices[2]
+
+								Tx = 0.5*y[m1,m2]*z[n1,n2]
+								Ty = 0.5*x[l1,l2]*z[n1,n2]
+								Tz = 0.5*x[l1,l2]*y[m1,m2]
+							
+								Tx *= l1*l2*x[l1-1,l2-1] + 4*alphaA*alphaB*x[l1+1,l2+1] - 2*alphaA*l2*x[l1+1,l2-1] - 2*alphaB*l1*x[l1-1,l2+1]
+								Ty *= m1*m2*y[m1-1,m2-1] + 4*alphaA*alphaB*y[m1+1,m2+1] - 2*alphaA*m2*y[m1+1,m2-1] - 2*alphaB*m1*y[m1-1,m2+1]
+								Tz *= n1*n2*z[n1-1,n2-1] + 4*alphaA*alphaB*z[n1+1,n2+1] - 2*alphaA*n2*z[n1+1,n2-1] - 2*alphaB*n1*z[n1-1,n2+1]
+
+								self.T[icount,jcount] += KK*Ca*Cb*(Tx+Ty+Tz)
+
+						jcount += 1
+				icount += 1
+
+		return self.T
 
 
 	def computeDipole(self):
@@ -194,11 +170,11 @@ class OS(object):
 								kappa  = np.exp( -zeta*np.linalg.norm(A-B)**2 )
 								KK     = kappa*(np.pi/alpha)**(3/2)
 
-								XYZ    = self.osRecursion(P-A,P-B,alpha,aShell.am+1,bShell.am+1)
+								XYZ    = self.os.recursion(P-A,P-B,alpha,aShell.am+1,bShell.am+1)
 								x,y,z  = XYZ[0], XYZ[1], XYZ[2]
 
-								aIndices = self.fillIndices(aShell.am)[i]
-								bIndices = self.fillIndices(bShell.am)[j]
+								aIndices = self.os.fillIndices(aShell.am)[i]
+								bIndices = self.os.fillIndices(bShell.am)[j]
 								l1, l2   = aIndices[0], bIndices[0]
 								m1, m2   = aIndices[1], bIndices[1]
 								n1, n2   = aIndices[2], bIndices[2]
@@ -221,7 +197,39 @@ class OS(object):
 		return np.linalg.norm(MU)
 
 
-	def osRecursion( self, PA, PB, alpha, AMa, AMb ):
+class OS:
+
+	def __init__(self,basis,molecule,uhfObject):
+
+		self.basis = basis
+		self.mol   = molecule
+		self.uhf   = uhfObject
+
+		print('The number of shells: %d' % (basis.nshell()))
+		print('The number of basis functions: %d' % (basis.nao()))
+
+		for ishell in range(basis.nshell()):
+			shell = basis.shell(ishell)
+			print('Shell %d:' % (ishell))
+			print('  Atom: %d' % (shell.ncenter))
+			print('  AM: %d' % (shell.am))
+			print('  # Cartesian functions: %d' % (shell.ncartesian))
+			print('  # of primitive Gaussians: %d ' % (shell.nprimitive))
+			print('  function_index: %d' % (shell.function_index))
+			print('  center: %f, %f, %f' % (molecule.x(shell.ncenter), molecule.y(shell.ncenter), molecule.z(shell.ncenter)))
+
+
+	def fillIndices(self,AM):
+		
+		indexObject = amIndices(AM)
+		indexArray  = indexObject.indices
+		indexList   = []
+		for row in indexArray:
+			indexList.append( [ int(i) for i in row ] )
+		return indexList
+
+
+	def recursion( self, PA, PB, alpha, AMa, AMb ):
 
 		xyz   = []
 		for i in range(3):
@@ -258,9 +266,13 @@ if __name__ == '__main__':
 	basis = psi4.core.BasisSet.build(mol, 'BASIS', config['DEFAULT']['basis'], puream=0)
 	mints = psi4.core.MintsHelper(basis)
 
-	uhf       = UHF(mol,mints)
-	integrals = OS(basis,mol,uhf)
+	scf   = config['SCF']
+	maxit = scf['max_iter']
+	conv  = scf['conv']
 
-	print( integrals.computeOverlap() - uhf.S)
-	print( integrals.computeKinetic() - uhf.T )
+	uhf       = UHF(mol,mints,maxit,conv)
+	integrals = Integrals(basis,mol,uhf)
+
+	#print( integrals.computeOverlap() - uhf.S)
+	#print( integrals.computeKinetic() - uhf.T )
 	print( "Dipole moment= ", integrals.computeDipole() )
